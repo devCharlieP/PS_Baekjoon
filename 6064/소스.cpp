0f676b24afc8b3d95a304f47cf71285592acf311
devCharlieP <rjsgud444@gmail.com>
2022-03-29 17:53:39 +0900

    commit

--- 6064/소스.cpp
+++ 6064/소스.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-
-
+#include <vector>
+#include <string>
+#include <cmath>
+#define endl '\n'
 using namespace std;
 
+int gcd(int M, int N)
+{
+	if (N == 0)
+	{
+		return M;
+	}
+	else
+	{
+		return gcd(N, M % N);
+	}
+}
 int main()
 {
 	cin.tie(NULL);
@@ -12,148 +24,42 @@ int main()
 
 	int T;
 	cin >> T;
-
+	
 	for (int i = 1; i <= T; i++)
 	{
-		int m, n, x, y;
-		cin >> m >> n >> x >> y;
-	
-		vector<pair<int, int>> vec(1);
+		int M, N, x, y;
+		cin >> M >> N >> x >> y;
 
-		if (m < n)
+		int res = 0;
+		bool b = false;
+		int lcm = 0;
+
+		if (M >= N)
 		{
-			vec.push_back(make_pair(0, m));
-			vec.push_back(make_pair(-m, n - m));
-
-			int cnt = 3;
-			int gap = n - m;
-			int num_x = m - n;
-			int num_y = n - m;
-
-			while (vec[cnt].first == (m - n))
-			{
-				//È¦¼ö
-				if (cnt % 2 != 0)
-				{
-					if (vec[cnt - 2].second + num_x == -1)
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, 1));
-						num_x = n - m;
-					}
-					else
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, vec[cnt - 2].second + num_x));
-					}
-				}
-				//Â¦¼ö
-				else
-				{
-					if (vec[cnt - 2].second + num_y > min(m, n))
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, min(m, n)));
-
-					}
-					else if (vec[cnt - 2].second == min(m, n))
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, vec[cnt - 4].second));
-						num_y = m - n;
-					}
-					else
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, vec[cnt - 2].second + num_y));
-					}
-				}
-			}
-
-			cnt = 1;
-			int sum = 0;
-
-			while (1)
-			{
-				if (cnt == vec.size())
-				{
-					cout << -1 << "\n";
-					break;
-				}
-				if (vec[cnt].first == (x - y))
-				{
-					cout << sum + min(x, y) << "\n";
-					break;
-				}
-
-				cnt++;
-				sum += vec[cnt].second;
-			}
-		}
-		else if(m > n)
-		{
-			vec.push_back(make_pair(0, n));
-			vec.push_back(make_pair(n, m - n));
-
-			int cnt = 3;
-			int gap = n - m;
-			int num_x = n - m;
-			int num_y = m - n;
-
-			while (vec[cnt].first == (m - n))
-			{
-				//È¦¼ö
-				if (cnt % 2 != 0)
-				{
-					if (vec[cnt - 2].second + num_x == -1)
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, 1));
-						num_x = m - n;
-					}
-					else
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, vec[cnt - 2].second + num_x));
-					}
-				}
-				//Â¦¼ö
-				else
-				{
-					if (vec[cnt - 2].second + num_y > min(m, n))
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, min(m, n)));
-
-					}
-					else if(vec[cnt - 2].second == min(m, n))
-					{
-						vec.push_back(make_pair(vec[cnt-2].first + gap, vec[cnt - 4].second));
-						num_y = n - m;
-					}
-					else
-					{
-						vec.push_back(make_pair(vec[cnt - 2].first + gap, vec[cnt - 2].second + num_y));
-					}
-				}
-			}
-
-			cnt = 1;
-			int sum = 0;
-
-			while (1)
-			{
-				if (cnt == vec.size())
-				{
-					cout << -1 << "\n";
-					break;
-				}
-				if (vec[cnt].first == (x - y))
-				{
-					cout << sum + min(x, y) << "\n";
-					break;
-				}
-
-				cnt++;
-				sum += vec[cnt].second;
-			}
-			
+			lcm = M * N / (gcd(M, N));
 		}
 		else
 		{
-			cout << m << "\n";
+			lcm = M * N / (gcd(N, M));
+		}
+
+		for (int i = 1; i <= lcm; i++)
+		{
+			if (i % M == x && i % N == y)
+			{
+				res = i;
+				b = true;
+				break;
+			}
+		}
+
+		if (b == true)
+		{
+			cout << res << endl;
+		}
+		else
+		{
+			cout << -1 << endl;
 		}
 	}
 

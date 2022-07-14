a2072a86cec12098a0ad0d031ae6702a09dd77b6
devCharlieP <rjsgud444@gmail.com>
2022-07-14 22:55:46 +0900

    commit

--- 10026/소스.cpp
+++ 10026/소스.cpp
@@ -11,7 +11,7 @@ vector <string> RGB(101);
 vector <vector <int>> ch(101, vector <int>(101));
 vector <int> di = {-1, 1, 0, 0};
 vector <int> dj = {0, 0, -1, 1};
-int res = 0;
+int res1 = 0, res2 = 0;
 
 void dfs(int Ai, int Aj, char C)
 {
@@ -23,7 +23,7 @@ void dfs(int Ai, int Aj, char C)
 		Adi = Ai + di[i];
 		Adj = Aj + dj[i];
 
-		if (Adi >= 1 && Adi <= N && Adj >= 1 && Adj <= N)
+		if (Adi >= 0 && Adi < N && Adj >= 0 && Adj < N)
 		{
 			if (C == RGB[Adi][Adj] && ch[Adi][Adj] == 0)
 			{
@@ -32,8 +32,6 @@ void dfs(int Ai, int Aj, char C)
 			}
 		}
 	}
-
-	res++;
 }
 
 int main()
@@ -43,24 +41,51 @@ int main()
 
 	cin >> N;
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		cin >> RGB[i];
 	}
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 1; j <= N; j++)
+		for (int j = 0; j < N; j++)
 		{
 			if (ch[i][j] == 0)
 			{
 				ch[i][j] = 1;
 				dfs(i, j, RGB[i][j]);
+				res1++;
 			}
 		}
 	}
 
-	cout << res;
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (RGB[i][j] == 'G')
+			{
+				RGB[i][j] = 'R';
+			}
+		}
+	}
+
+	ch = vector <vector <int>>(101, vector <int>(101));
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (ch[i][j] == 0)
+			{
+				ch[i][j] = 1;
+				dfs(i, j, RGB[i][j]);
+				res2++;
+			}
+		}
+	}
+
+	cout << res1 << " " << res2;
 
 	return 0;
 }

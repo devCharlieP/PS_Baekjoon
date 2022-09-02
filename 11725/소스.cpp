349a6b5961a6478c00f3b98204e09662b7553e13
devCharlieP <rjsgud444@gmail.com>
2022-09-02 16:52:55 +0900

    commit

---
+++ 11725/소스.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <queue>
+#define endl '\n'
+using namespace std;
+
+int N;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	cin >> N;
+	
+	for (int i = 1; i <= N - 1; i++)
+	{
+		int x, y;
+		cin >> x >> y;
+	}
+
+
+	return 0;
+}

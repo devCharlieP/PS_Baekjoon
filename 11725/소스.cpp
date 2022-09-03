9f194bac5e9fc6e07921ffcb494bcc51c2ff34fc
devCharlieP <rjsgud444@gmail.com>
2022-09-03 17:07:39 +0900

    commit

--- 11725/소스.cpp
+++ 11725/소스.cpp
@@ -8,6 +8,9 @@
 using namespace std;
 
 int N;
+vector <int> res(100001);
+vector <int> tree[100001];
+vector <int> ch(100001);
 
 int main()
 {
@@ -20,8 +23,37 @@ int main()
 	{
 		int x, y;
 		cin >> x >> y;
+		tree[x].push_back(y);
+		tree[y].push_back(x);
 	}
 
+	queue <int> Q;
+	Q.push(1);
+
+	while (!Q.empty())
+	{
+		int front = Q.front();
+		Q.pop();
+		
+		ch[front] = 1;
+
+		for (int i = 0; i < tree[front].size(); i++)
+		{
+			int tree_Front = tree[front][i];
+
+			if (ch[tree_Front] == 0)
+			{
+				ch[tree_Front] = 1;
+				res[tree_Front] = front;
+				Q.push(tree_Front);
+			}
+		}
+	}
+
+	for (int i = 2; i <= N; i++)
+	{
+		cout << res[i] << endl;
+	}
 
 	return 0;
 }

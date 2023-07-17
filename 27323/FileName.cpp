5434c590f432860bb781311b572a0d10c6dccf52
devCharlieP <rjsgud444@gmail.com>
2023-07-17 21:26:45 +0900

    commit

---
+++ 27323/FileName.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+
+	return 0;
+}

12e83548a7b52da8029b5dc2cc4d4f5284fbb8b7
devCharlieP <rjsgud444@gmail.com>
2023-07-19 00:53:29 +0900

    commit

---
+++ 15894/FileName.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	long long n;
+	cin >> n;
+
+	cout << 4 * n;
+
+	return 0;
+}

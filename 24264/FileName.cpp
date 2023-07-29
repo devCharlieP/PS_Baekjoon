7f38842992b25ab0944f042b08bc6aa931b996d9
devCharlieP <rjsgud444@gmail.com>
2023-07-29 22:23:25 +0900

    commit

---
+++ 24264/FileName.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	long long n;
+	cin >> n;
+
+	cout << n * n << endl << 2;
+
+	return 0;
+}

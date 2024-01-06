fc2b1cee0175ba5fb86023a9026a13fd862897da
devCharlieP <rjsgud444@gmail.com>
2024-01-06 21:03:42 +0900

    commit

---
+++ 24723/FileName.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	cout << pow(2, N);
+
+	return 0;
+}

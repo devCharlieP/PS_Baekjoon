0358dfb7ba1ae296a127cd69dfc63ccad2d26311
devCharlieP <rjsgud444@gmail.com>
2024-01-09 23:28:25 +0900

    commit

---
+++ 26069/FileName.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	map <string, int> ch;
+	ch.insert({ "ChongChong", 0 });
+
+	string s1, s2;
+
+	for (int i = 1; i <= N; i++)
+	{
+		cin >> s1 >> s2;
+
+		if (ch.find(s1) != ch.end())
+		{
+			ch.insert({s2, 0});
+		}
+		else if(ch.find(s2) != ch.end())
+		{
+			ch.insert({ s1, 0 });
+		}
+	}
+
+	cout << ch.size();
+
+	return 0;
+}

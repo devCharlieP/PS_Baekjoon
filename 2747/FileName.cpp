f1bc1e1be2e55df64a7735121a47aa1e62f8fc91
devCharlieP <rjsgud444@gmail.com>
2024-10-07 21:48:02 +0900

    commit

---
+++ 2747/FileName.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define endl "\n"
+using namespace std;
+
+int main()
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	vector<int> vec(n + 1);
+	vec[0] = 0;
+	vec[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		vec[i] = vec[i - 1] + vec[i - 2];
+	}
+
+	cout << vec[n];
+
+	return 0;
+}

8a390e31fa70fe13918886ee55cca9d72283ca6f
devCharlieP <rjsgud444@gmail.com>
2025-03-18 15:30:05 +0900

    commit

--- 18111/소스.cpp
+++ 18111/소스.cpp
@@ -2,86 +2,78 @@
 #include <algorithm>
 #include <vector>
 #include <string>
-#include <cmath>
-#define endl '\n'
+#include <climits>
+#define endl "\n"
 using namespace std;
 
-
-int N, M, B;
-vector <vector <int>> vec(501, vector<int>(501));
-
-void dfs(int k, int l, int inv)
-{
-	int ch_temp = vec[0][0];
-	bool ch_bool = true;
-
-	for (int i = 0; i < N; i++)
-	{
-		for (int j = 0; j < M; j++)
-		{
-			if (ch_temp != vec[i][j])
-			{
-				ch_bool = false;
-				break;
-			}
-		}
-
-		if (ch_bool == false)
-		{
-			break;
-		}
-	}
-
-	if (ch_bool == false)
-	{
-		if (vec[k][l] == 0)
-		{
-			vec[k][l]++;
-			dfs(k, l, inv - 1);
-			vec[k][l]--;
-
-			if (k == N - 1)
-			{
-				dfs(k + 1, 0, inv);
-			}
-			else
-			{
-				dfs(k, l + 1, inv);
-			}
-		}
-		else if (vec[k][l] == 256)
-		{
-
-		}
-		else
-		{
-
-		}
-			
-	}
-	else
-	{
-
-	}
-
-	
-}
 int main()
 {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
 
+	int N, M, B;
 	cin >> N >> M >> B;
+
+	int maxHeight = B;
+	vector<vector<int>> map(N, vector<int>(M));
 
 	for (int i = 0; i < N; i++)
 	{
 		for (int j = 0; j < M; j++)
 		{
-			cin >> vec[i][j];
+			cin >> map[i][j];
+			maxHeight += map[i][j];
 		}
 	}
 
-	dfs(0, 0, B);
+	maxHeight /= (N * M);
+
+	if (maxHeight > 256)
+	{
+		maxHeight = 256;
+	}
+
+	vector<int> res(maxHeight + 1);
+
+	for (int height = 0; height <= maxHeight; height++)
+	{
+		int timeSum = 0;
+
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < M; j++)
+			{
+				int time = 0;
+
+				if (map[i][j] < height)
+				{
+					time = height - map[i][j];
+				}
+				else if (map[i][j] > height)
+				{
+					time = 2 * (map[i][j] - height);
+				}
+
+				timeSum += time;
+			}
+		}
+
+		res[height] = timeSum;
+	}
+
+	int resTime = INT_MAX;
+	int resHeight = 0;
+
+	for (int i = 0; i < res.size(); i++)
+	{
+		if (res[i] <= resTime)
+		{
+			resTime = res[i];
+			resHeight = i;
+		}
+	}
+
+	cout << resTime << " " << resHeight;
 
 	return 0;
 }
